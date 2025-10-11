f0c65b989682fc5663a486b9d504be67bf4df687
ClozeeFF <Fearless-clozee@hotmail.com>
2025-10-11 21:09:53 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,22 +14,3 @@
     Discord: https://discord.gg/ftgs-development-hub-1300692552005189632
     License: MIT
 ]]
-
-
-local a a={cache={}, load=function(b)if not a.cache[b]then a.cache[b]={c=a[b]()}end return a.cache[b].c end}do function a.a()local b=game:GetService"RunService"local d=
-b.Heartbeat
-local e=game:GetService"UserInputService"
-local f=game:GetService"TweenService"
-local g=game:GetService"LocalizationService"
-local h=game:GetService"HttpService"
-
-local i="https://raw.githubusercontent.com/Footagesus/Icons/main/Main-v2.lua"
-
-local j=loadstring(
-game.HttpGetAsync and game:HttpGetAsync(i)
-or h:GetAsync(i)
-)()
-j.SetIconsType"lucide"
-
-local l
-
