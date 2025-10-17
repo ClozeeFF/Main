a3c28c73716ffca79f7bf4c19b41948e351a7262
ClozeeFF <Fearless-clozee@hotmail.com>
2025-10-18 01:39:24 +0700

    Update EggSelection.lua

--- EggSelection.lua
+++ EggSelection.lua
@@ -145,22 +145,22 @@ local MutationData = {
     Golden = {
         Name = "Golden",
         Icon = "rbxassetid://127002529028834",
-        Rarity = 20
+        Rarity = 10
     },
     Diamond = {
         Name = "Diamond", 
         Icon = "rbxassetid://121197098708770",
-        Rarity = 30
+        Rarity = 20
     },
     Electirc = {
         Name = "Electric",
         Icon = "rbxassetid://117484832890686",
-        Rarity = 40
+        Rarity = 50
     },
     Fire = {
         Name = "Fire",
         Icon = "rbxassetid://83433633155964",
-        Rarity = 50
+        Rarity = 100
     },
     Jurassic = {
         Name = "Jurassic",
